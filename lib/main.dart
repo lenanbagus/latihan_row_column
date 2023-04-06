dc915559b965e479c86c4951b05396584d9a14a0
lenanbagus <lenanbagus@outlook.com>
2023-04-06 14:08:20 +0700

    adding image and recenter icon

--- lib/main.dart
+++ lib/main.dart
@@ -23,80 +23,104 @@ class HalamanDesain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.greenAccent,
       appBar: AppBar(
         title: const Text('row dan column'),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.blueAccent,
       ),
       body: Column(
         children: [
           Container(
             margin: const EdgeInsets.all(10),
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(10)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Column(
-                  children: const [
-                    Icon(
-                      Icons.home,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    Text('Home')
-                  ],
-                ),
-                Column(
-                  children: const [
-                    Icon(
-                      Icons.edit,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    Text('Edit')
-                  ],
-                ),
-                Column(
-                  children: const [
-                    Icon(
-                      Icons.save,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    Text('Save')
-                  ],
-                ),
-                Column(
-                  children: const [
-                    Icon(
-                      Icons.abc,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    Text('Type')
-                  ],
-                ),
-              ],
+            width: 800,
+            height: 60,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        size: 30,
+                        color: Colors.blue,
+                      ),
+                      Text('Home')
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.edit,
+                        size: 30,
+                        color: Colors.blue,
+                      ),
+                      Text('Edit')
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.save,
+                        size: 30,
+                        color: Colors.blue,
+                      ),
+                      Text('Save')
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.abc,
+                        size: 30,
+                        color: Colors.blue,
+                      ),
+                      Text('Type')
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
-            margin: const EdgeInsets.all(10),
-            width: 600,
-            height: 200,
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-          ),
+              margin: const EdgeInsets.all(10),
+              width: 600,
+              height: 200,
+              decoration: const BoxDecoration(
+                  color: Colors.blue, shape: BoxShape.circle),
+              child: Image.network(
+                  'https://lenanlogic.com/assets/img/portfolio/fullsize/2.jpg')),
           Container(
-            margin: const EdgeInsets.all(10),
-            width: 600,
-            height: 200,
-            decoration: const BoxDecoration(
-                color: Colors.white, shape: BoxShape.circle),
-          ),
+              margin: const EdgeInsets.all(10),
+              child: const Text('This Image contain a copyright')),
+          Container(
+              margin: const EdgeInsets.all(10),
+              width: 600,
+              height: 200,
+              decoration: const BoxDecoration(
+                  color: Colors.blue, shape: BoxShape.circle),
+              child: Image.network(
+                  'https://lenanlogic.com/assets/img/portfolio/fullsize/2.jpg')),
+          Container(
+              margin: const EdgeInsets.all(10),
+              child: const Text('This Image contain a copyright')),
+          Container(
+              margin: const EdgeInsets.all(10),
+              width: 600,
+              height: 200,
+              decoration: const BoxDecoration(
+                  color: Colors.blue, shape: BoxShape.circle),
+              child: Image.network(
+                  'https://lenanlogic.com/assets/img/portfolio/fullsize/2.jpg')),
+          Container(
+              margin: const EdgeInsets.all(10),
+              child: const Text('This Image contain a copyright')),
         ],
       ),
     );
